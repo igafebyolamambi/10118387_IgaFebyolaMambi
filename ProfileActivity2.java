791ac82ccbbc818b35de885111c7316c70e05338
By.ifbyla <62938221+igafebyolamambi@users.noreply.github.com>
2021-06-01 18:19:33 +0700

    UTS AKB

---
+++ ProfileActivity2.java
@@ -0,0 +1,14 @@
+package com.example.uts_akb_10118387_if_9_igafebyolamambi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile2);
+    }
+}
